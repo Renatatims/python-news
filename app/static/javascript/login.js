5e9d5020403a8cf14bad4fa9db056985b64e64d5
ReTims <renatatims@gmail.com>
2023-03-20 13:17:47 -0500

    Included signup form + signup js logic

--- app/static/javascript/login.js
+++ app/static/javascript/login.js
@@ -1,3 +1,4 @@
+// Login Function
 async function loginFormHandler(event) {
     event.preventDefault();
   
@@ -22,4 +23,34 @@ async function loginFormHandler(event) {
     }
   }
 
+  // Signup function
+
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#inputSignupUsername').value.trim();
+    const email = document.querySelector('#inputSignupEmail').value.trim();
+    const password = document.querySelector('#inputSignupPassword').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
