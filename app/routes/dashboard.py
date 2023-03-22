40dd89b55a02e74e888268f25435d47632ce68be
ReTims <renatatims@gmail.com>
2023-03-22 01:34:24 -0500

    Connected the dash route to the database and query it - dashboard.py - now user's posts are rendered to the dashboard as well

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from app.models import Post
+from app.db import get_db
 
 # Dashboard routes
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
@@ -6,7 +8,19 @@ bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 # /dashboard
 @bp.route('/')
 def dash():
-  return render_template('dashboard.html')
+  # User's posts are displayed in the dashboard
+  db = get_db()
+  posts = (
+    db.query(Post)
+    .filter(Post.user_id == session.get('user_id'))
+    .order_by(Post.created_at.desc())
+    .all()
+  )
+  return render_template(
+    'dashboard.html',
+    posts=posts,
+    loggedIn=session.get('loggedIn')
+  )
 
 # /dashboard/edit/<id>
 @bp.route('/edit/<id>')
