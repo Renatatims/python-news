86828c7af04b8dea78257cfa138c884b1836d2eb
ReTims <renatatims@gmail.com>
2023-03-30 12:37:07 -0500

    Import the login_required decorator to dashboard.py - dashboard and edit routes and api.py - to create, update, delete, upvote and comment routes

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, render_template, session
 from app.models import Post
 from app.db import get_db
+from app.utils.auth import login_required
 
 # Dashboard routes
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 # /dashboard
 @bp.route('/')
+@login_required
 def dash():
   # User's posts are displayed in the dashboard
   db = get_db()
@@ -24,6 +26,7 @@ def dash():
 
 # Edit posts by id - /dashboard/edit/<id>
 @bp.route('/edit/<id>')
+@login_required
 def edit(id):
   # get single post by id
   db = get_db()
